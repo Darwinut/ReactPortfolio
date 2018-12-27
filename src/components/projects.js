9ea36fbaf758619cd23f5219e6ae003c704911ae
Darwin <darwin4017@gmail.com>
2018-12-27 23:22:44 +0200

    Updated projects tab

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import { Tabs, Tab } from 'react-mdl';
 
-
 class Projects extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { activeTab: 0};
 	}
+   
+   	togglecategories () {
+		if(this.state.activeTab === 0) {
+		return(
+			<div><h1> This is React</h1></div>
+		)
+		}
 
+	}
+
+	
 	render () {
 		return (
 		<div className="category-tabs">
-		  <Tabs activeTab={this.state.activeTab} onChange={(tabID) => this.setState({ activeTab: tabId })} ripple>
-		  <Tab>Selenium</Tab>
-		  <Tab>React</Tab>
-		  <Tab>Other</Tab>
-		  </Tabs>
-		</div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>Selenium</Tab>
+          <Tab>TestNG</Tab>
+          <Tab>Cucumber</Tab>
+          <Tab>React</Tab>
+        </Tabs>
+        </div>
 		)
 	}
 }
