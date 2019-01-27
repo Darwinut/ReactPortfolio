da01c11f45626b48d152fdcffd823c4884c596c4
Darwin <darwin4017@gmail.com>
2019-01-27 04:27:29 +0200

    Added project cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
 	constructor(props) {
@@ -12,7 +12,58 @@ class Projects extends Component {
 		
 	  if(this.state.activeTab === 0){
 		return(
-			<div><h1> This is Selenium</h1></div>
+			
+			<div className="projects-grid">
+      {/* Project 1 */}
+		 	<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+			  <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.seleniumhq.org/images/big-logo.png) center / cover'}}>Selenium Project #1</CardTitle>
+			  <CardText>
+			  Lorem Ipsum is simply dummy text of the printing and typesetting industry 
+			  </CardText>
+			  <CardActions border>
+			    <Button coloured>Github</Button>
+			    <Button coloured>CodePen</Button>
+			    <Button coloured>LiveDemo</Button>
+			  </CardActions>
+			    <CardMenu style={{color: '#fff'}}>
+			      <IconButton name="share" />
+			    </CardMenu>
+			</Card>  
+
+    {/* Project 2 */}
+      <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://1.bp.blogspot.com/-aXHbB9nPPeI/VUZUwaKi8TI/AAAAAAAAAr0/yiXtd5WALPo/s1600/cucumber.png) center / cover'}}>Selenium Project #2</CardTitle>
+        <CardText>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry 
+        </CardText>
+        <CardActions border>
+          <Button coloured>Github</Button>
+          <Button coloured>CodePen</Button>
+          <Button coloured>LiveDemo</Button>
+        </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+      </Card> 
+
+    {/* Project 3 */}
+      <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.seleniumhq.org/images/big-logo.png) center / cover'}}>Selenium Project #3</CardTitle>
+        <CardText>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry 
+        </CardText>
+        <CardActions border>
+          <Button coloured>Github</Button>
+          <Button coloured>CodePen</Button>
+          <Button coloured>LiveDemo</Button>
+        </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+      </Card> 
+      </div>
+
+
 		)
 	  } else if(this.state.activeTab === 1) {
 	  	return (
